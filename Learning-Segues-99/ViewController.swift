ef023a1e04b35abd2f92b734193747ef8737aa3a
Can Baltaci <canbaltaci@Cans-MBP.local>
2023-08-05 07:46:14 +0200

    Initial Commit

---
+++ Learning-Segues-99/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Learning-Segues-99
+//
+//  Created by Can BALTACI on 05.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
